76dab95d347fe305aaf26cb865d449fd795235f0
plopeztt <plopez@ttagro.cl>
2026-07-15 10:00:06 -0400

    Add authentication middleware to handle requests
    Implement basic authentication middleware for requests.

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,1 +1,26 @@
 
+export async function onRequest(context) {
+  const { request } = context;
+  const authHeader = request.headers.get('Authorization');
+  
+  const USERNAME = 'admin'; 
+  const PASSWORD = 'ttagro26'; 
+
+  if (authHeader) {
+    const [scheme, encoded] = authHeader.split(' ');
+    if (scheme === 'Basic') {
+      const decoded = atob(encoded);
+      const [user, pass] = decoded.split(':');
+      if (user === USERNAME && pass === PASSWORD) {
+        return context.next(); // Deja pasar a la web
+      }
+    }
+  }
+
+  return new Response('Acceso restringido', {
+    status: 401,
+    headers: {
+      'WWW-Authenticate': 'Basic realm="Acceso Privado"',
+    },
+  });
+}
